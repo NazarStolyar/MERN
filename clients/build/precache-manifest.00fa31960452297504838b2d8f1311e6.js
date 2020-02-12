79e614bdb697d44365bc26b8dd92f543dbea78a4
NazarStolyar <batinak94@gmail.com>
2020-02-12 11:18:46 +0200

    build

---
+++ clients/build/precache-manifest.00fa31960452297504838b2d8f1311e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c927d3076372d92bf30e8ba6b4dff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d58d67bf82cb2d21ed10",
+    "url": "/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "99bb6746200613d6f6c7",
+    "url": "/static/css/main.450d00b3.chunk.css"
+  },
+  {
+    "revision": "d58d67bf82cb2d21ed10",
+    "url": "/static/js/2.81f4001a.chunk.js"
+  },
+  {
+    "revision": "ef106feaf9d5e6d92f08d74f9482435a",
+    "url": "/static/js/2.81f4001a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99bb6746200613d6f6c7",
+    "url": "/static/js/main.1d1eede4.chunk.js"
+  },
+  {
+    "revision": "962e3726437713de13a5",
+    "url": "/static/js/runtime-main.efdde5a0.js"
+  }
+]);
