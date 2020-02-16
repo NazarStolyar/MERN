d8113ea34b82153204c5e853cfe72aa04f11a4e6
NazarStolyar <batinak94@gmail.com>
2020-02-16 19:23:05 +0200

    build

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('config');
-const path = require('path');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 app.use(express.json( { extended: true } ));
@@ -9,15 +9,15 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/',  express.static(path.join(__dirname, 'client', 'build')));
 
-    app.use('*', async (req, res) => {
+const PORT = config.get('port') || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
-
-const PORT = config.get('port') || 5000;
 
 async function start () {
     try {
